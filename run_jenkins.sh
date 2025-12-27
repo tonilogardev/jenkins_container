6efd826b2f9b38df8370bf1e9405dcdeb1ad7d53
tonilogar <tologar@gmail.com>
2025-12-27 19:47:55 +0100

    Update run_jenkins.sh to stop containers before starting

--- run_jenkins.sh
+++ run_jenkins.sh
@@ -6,6 +6,9 @@ set -e
 # Clean up Docker
 docker system prune -f --volumes 2>/dev/null || true
 
+# Stop runing containers
+docker compose down
+
 # Build and start (logs visible, Ctrl+C stops container)
 docker compose build
 docker compose up
